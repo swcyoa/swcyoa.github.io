09c93afabcd127c4247738c5680100464efa887c
swcyoa <85349569+swcyoa@users.noreply.github.com>
2021-06-04 14:54:32 -0300

    initialize js.js with four points variables
    and allow us allow us to make a html paragraph for the points variables too :)

---
+++ js.js
@@ -0,0 +1,8 @@
+var _pts1 = 0;//init points variable one
+var _pts2 = 0;//init points variable two
+var _pts3 = 0;//init points variable three
+var _pts4 = 0;//init points variable four
+document.getElementById("_pts1").innerHTML = _pts1;//allow us to make a html paragraph for the points variable one
+document.getElementById("_pts1").innerHTML = _pts2;//allow us to make a html paragraph for the points variable two
+document.getElementById("_pts1").innerHTML = _pts3;//allow us to make a html paragraph for the points variable three
+document.getElementById("_pts1").innerHTML = _pts4;//allow us to make a html paragraph for the points variable four
